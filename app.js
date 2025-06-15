391354e94e16f1da3ea17595a6a0996f7dced431
se-omar-ahmed <146363315+se-omar-ahmed@users.noreply.github.com>
2025-06-15 14:26:04 +0300

    Remove alert function

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/planet',   function(req, res) {
         id: req.body.id
     }, function(err, planetData) {
         if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
+            // alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
             res.send("Error in Planet Data")
         } else {
             res.send(planetData);
